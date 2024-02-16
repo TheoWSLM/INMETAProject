0192a847144ceb6b0e00c68d7e1659fc7f77a07d
TheoWSLM <theoweber1@gmail.com>
2024-02-16 14:04:05 -0300

    feat: implementação de navbar e footer

---
+++ src/components/FooterFixed.vue
@@ -0,0 +1,27 @@
+
+<template>
+<footer class=" rounded-lg shadow absolute w-full z-20 bottom-0 start-0">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            </a>
+            <ul class="flex flex-wrap items-center text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Como funciona?</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Sobre</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contatos</a>
+                </li>
+            </ul>
+        </div>
+        <hr class=" border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-4" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">2024, feito por Theo Weber</span>
+    </div>
+</footer>
+
+</template>
