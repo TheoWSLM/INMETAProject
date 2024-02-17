c74a7a4a72f40b48cc6a06d25c1661a81b37721b
TheoWSLM <theoweber1@gmail.com>
2024-02-17 18:27:48 -0300

    feat: novo componente da home

--- src/components/sectionItems/InformationAndDescriptionCards.vue
+++ src/components/sectionItems/InformationAndDescriptionCards.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
       <div
-        class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8"
+        class="  rounded-lg p-8 md:p-12 mb-8"
       >
         <a
           href="#"
@@ -50,7 +50,7 @@
         </a>
       </div>
 
-      <div class="grid md:grid-cols-3 gap-8">
+      <div class="grid md:grid-cols-4 gap-4">
         <div
           v-for="(card, cardIndex) in content[0].cards"
           :key="cardIndex"
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,76 @@
-<script setup></script>
+
 
 <template>
-  <main>home</main>
-</template>
+  <InformationAndDescriptionCards :content="dataArray" />
+  <AdvantagesCallToAction />
+  </template>
+
+
+<script >
+import InformationAndDescriptionCards from '@/components/sectionItems/InformationAndDescriptionCards.vue';
+import AdvantagesCallToAction from '@/components/sections/AdvantagesCallToAction.vue';
+
+export default{
+components:{
+    AdvantagesCallToAction,
+    InformationAndDescriptionCards
+},
+data() {
+    return {
+      dataArray: [
+        {
+          category: 'Tutorial',
+          title: 'How to quickly deploy a static website',
+          description:
+            'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.',
+          cards: [
+            {
+              category: 'Tutorial',
+              title: 'How to quickly deploy a static website',
+              description:
+                'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.',
+              button: ''
+            },
+            {
+              category: 'Design',
+              title: 'Start with Flowbite Design System',
+              description:
+                'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.',
+              button: ''
+            },
+            {
+              category: 'Code',
+              title: 'Best react libraries around the web',
+              description:
+                'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.',
+              button: ''
+            },
+            {
+              category: 'Tutorial',
+              title: 'How to quickly deploy a static website',
+              description:
+                'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.',
+              button: ''
+            },
+            {
+              category: 'Design',
+              title: 'Start with Flowbite Design System',
+              description:
+                'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.',
+              button: ''
+            },
+            {
+              category: 'Code',
+              title: 'Best react libraries around the web',
+              description:
+                'Static websites are now used to bootstrap lots of websites and are becoming the basis for a variety of tools that even influence both web designers and developers.',
+              button: ''
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
+
