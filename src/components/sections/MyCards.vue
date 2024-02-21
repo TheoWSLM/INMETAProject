b394547023b8e59473f3154a02380017e390e571
TheoWSLM <theoweber1@gmail.com>
2024-02-21 13:17:42 -0300

    feat: adição de filtro de busca

--- src/components/sections/MyCards.vue
+++ src/components/sections/MyCards.vue
@@ -1,28 +1,63 @@
 <template>
-   <TitleAndDescription
-        :title="'Suas cartas'"
-        :description="'Aqui estão todas as suas cartas registradas!'"
-      />
-      <div class="grid md:grid-cols-4 gap-16">
-  <CardList  v-for="(myCard, cardIndex) in myCards"  :key="cardIndex" :cardInfo="myCard" :buttonLabel="'Trocar'"/>
+  <TitleAndDescription
+       :title="'Suas cartas'"
+       :description="'Aqui estão todas as suas cartas registradas!'"
+     />
+
+     <SearchFilter
+     :searchTerm="searchTerm"
+     :afterFilterCards="afterFilterCards"
+     :filterCards="filterCards"
+     @update:searchTerm="handleSearchTermUpdate"
+   />
+     <div class="grid md:grid-cols-4 gap-16">
+      
+ <CardList  v-for="(myCard, cardIndex) in afterFilterCards"  :key="cardIndex" :cardInfo="myCard" :buttonLabel="'Trocar'"/>
 </div>
 </template>
 
 <script>
 import TitleAndDescription from '../sectionItems/TitleAndDescription.vue';
 import CardList from './CardListSection.vue';
+import SearchFilter from '@/components/sectionItems/SearchFilter.vue' 
 
 export default{
-  components:{
-    TitleAndDescription,
-    CardList
-  },
-  props: {
-    myCards: {
-      type: Object,
-      default: null
-    }
-  }
-}
-</script>
+ components:{
+   TitleAndDescription,
+   CardList,
+   SearchFilter
+ },
+ props: {
+   myCards: {
+     type: Object,
+   }
+ },
+ data() {
+   return {
+     searchTerm: '',
+     afterFilterCards: [] 
+   }
+ },
+ mounted(){
 
+   this.afterFilterCards = this.myCards;
+ },
+ methods:{
+   filterCards() {
+
+     if (!this.searchTerm) {
+
+       this.afterFilterCards = this.myCards;
+     } else {
+       this.afterFilterCards = this.myCards.filter(card =>
+         card.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+       );
+     }
+   },
+   handleSearchTermUpdate(newSearchTerm) {
+     this.searchTerm = newSearchTerm;
+     this.filterCards();
+   }
+ }
+};
+</script>
--- src/views/CardsView.vue
+++ src/views/CardsView.vue
@@ -1,28 +1,45 @@
 <template>
-   <TitleAndDescription
-        :title="'Cartas disponíveis'"
-        :description="'Aqui estão todas as cartas registradas!'"
+  <div>
+    <TitleAndDescription
+      :title="'Cartas disponíveis'"
+      :description="'Aqui estão todas as cartas registradas!'"
+    />
+    <SearchFilter
+      :searchTerm="searchTerm"
+      :afterFilterCards="afterFilterCards"
+      :filterCards="filterCards"
+      @update:searchTerm="handleSearchTermUpdate"
+    />
+
+    <div class="grid md:grid-cols-4 gap-16">
+      <CardList
+        v-for="(card, cardIndex) in afterFilterCards"
+        :key="cardIndex"
+        :cardInfo="card"
       />
-      <div class="grid md:grid-cols-4 gap-16">
-  <CardList  v-for="(card, cardIndex) in data"  :key="cardIndex" :cardInfo="card" />
-</div>
+    </div>
+  </div>
 </template>
+
 <script>
 import CardList from '@/components/sections/CardListSection.vue'
 import apiService from '@/services/apiService'
 import TitleAndDescription from '@/components/sectionItems/TitleAndDescription.vue'
+import SearchFilter from '@/components/sectionItems/SearchFilter.vue'
 
 export default {
   components: {
     CardList,
-    TitleAndDescription
-    
+    TitleAndDescription,
+    SearchFilter
   },
   data() {
     return {
       data: {
         cards: []
-      }
+      },
+      searchTerm: '',
+      afterFilterCards: []
     }
   },
   created() {
@@ -34,12 +51,34 @@ export default {
         const response = await this.allCards()
         console.log(response.data.list)
         this.data = response.data.list
+        this.afterFilterCards = this.data
       } catch (error) {
         console.error('Error fetching data:', error)
       }
     },
     async allCards() {
       return await apiService.allCards()
+    },
+    filterCards() {
+      // Filtrar as cartas com base no searchTerm
+      if (!this.searchTerm) {
+        this.afterFilterCards = this.data;
+      } else {
+        this.afterFilterCards = this.data.filter(card =>
+          card.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+        );
+      }
+    },
+    handleSearchTermUpdate(newSearchTerm) {
+      this.searchTerm = newSearchTerm;
+      this.filterCards();
+    }
+  },
+  computed: {
+    filteredCards() {
+      return this.data.filter(card =>
+        card.name.toLowerCase().includes(this.searchTerm.toLowerCase())
+      );
     }
   }
 }
